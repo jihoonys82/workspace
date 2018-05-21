a7e61ea9b6767ad74d1f9f2e37612a933cf26306
jihoonys82 <jihoonys82@gmail.com>
2018-05-21 10:57:33 +0900

    FileReadWrite Practice

--- Filestream/src/com/ji/filestream/FilestreamTest.java
+++ Filestream/src/com/ji/filestream/FilestreamTest.java
@@ -1,5 +1,21 @@
 package com.ji.filestream;
 
-public class FilestreamTest {
+import java.io.File;
 
+public class FilestreamTest {
+	public static void main(String[] args) {
+		File f = new File("C:\\Users\\Ji\\Desktop\\test.txt");
+		
+		if(f.exists()) {
+			f.deleteOnExit();
+			System.out.println(f.getName());
+			System.out.println(f.getPath());
+			System.out.println(f.getParent());
+			System.out.println(f.length());
+		} else {
+			System.out.println("file not found");
+		}
+		
+		
+	}
 }
